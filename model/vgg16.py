c83828e4c6833223518ff93b53666b448ed4e030
HikariMusic <hikarimusic.tm@gmail.com>
2022-09-20 22:03:40 +0800

    Update and rename VGG16.py to vgg16.py

--- model/vgg16.py
+++ model/vgg16.py
@@ -172,14 +172,13 @@ class Model(nn.Module):
 class Utils():
     def __init__(self):
         super().__init__()
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
+        self.transform = nn.Sequential(
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
+        )
         self.target_transform = None
         self.loss = nn.CrossEntropyLoss()
     
     def result(self, x):
-        return torch.argsort(x, dim=1)
+        return torch.argmax(x, dim=1)
