48598fedd7eb51722ff51abd5490181da73f6ea7
HikariMusic <hikarimusic.tm@gmail.com>
2022-09-20 22:04:46 +0800

    Create image_classification.py

---
+++ image_classification.py
@@ -0,0 +1,72 @@
+from data.cifar10 import TrainDataset
+from model.vgg16 import Model, Utils
+
+import os
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+LOAD_WEIGHT = None
+SAVE_WEIGHT = os.path.join(os.getcwd(), "weight", "vgg.pth")
+
+BATCH_SIZE = 64
+EPOCHS = 20
+OPTIMIZER = torch.optim.SGD
+LEARN_RATE = 1e-3
+
+def train(data, model, utils):
+    print("Training")
+    model.train()
+    size = len(data.dataset)
+    for batch, (X, y) in enumerate(data):
+        X, y = X.to(DEVICE), y.to(DEVICE)
+        X = utils.transform(X) if utils.transform else X
+        y = utils.target_transform(y) if utils.target_transform else y
+        _y = model(X)
+        loss = utils.loss(_y, y)
+        optimizer = OPTIMIZER(model.parameters(), lr=LEARN_RATE)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+            torch.save(model.state_dict(), SAVE_WEIGHT)
+
+def test(data, model, utils):
+    print("Testing")
+    model.eval()
+    size = len(data.dataset)
+    num_batches = len(data)
+    loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in tqdm(data):
+            X, y = X.to(DEVICE), y.to(DEVICE)
+            X = utils.transform(X) if utils.transform else X
+            y = utils.target_transform(y) if utils.target_transform else y
+            _y = model(X)
+            pred = utils.result(_y)
+            loss += utils.loss(_y, y).item()
+            correct += (pred == y).type(torch.float).sum().item()
+    loss /= num_batches
+    correct /= size
+    print(f"Accuracy: {(100*correct):>0.1f}%, Avg. Loss: {loss:>8f} \n")
+
+
+if __name__ == '__main__':
+    dataset = TrainDataset()
+    data = DataLoader(dataset, batch_size=BATCH_SIZE)
+    model = Model()
+    utils = Utils()
+    if LOAD_WEIGHT:
+        model.load_state_dict(torch.load(LOAD_WEIGHT))
+
+    for t in range(EPOCHS):
+        print(f"\nEpoch {t+1}\n-------------------------------")
+        train(data, model, utils)
+        eval(data, model, utils)
+        if SAVE_WEIGHT:
+            torch.save(model.state_dict(), SAVE_WEIGHT)
